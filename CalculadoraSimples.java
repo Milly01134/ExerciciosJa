eb30308218d59e0a8649cbdfadec8d72858861a9
Camilly <camilly.taq@gmail.com>
2026-05-25 20:15:51 -0300

    Add files via upload

---
+++ CalculadoraSimples.java
@@ -0,0 +1,40 @@
+import java.util.Scanner; //sempr tem que fazer isso para funcionar
+
+public class CalculadoraSimples{
+    public static void main (String []args) {
+      
+        Scanner le = new Scanner (System.in);
+
+        System.out.println ("Digite um número de 1 a 10:  ");
+        int num1 = le.nextInt();
+
+        System.out.println ("Digite outro número de 1 a 10:  ");
+        int num2 = le.nextInt();
+
+        System.out.println ("Digite uma das quatro operações = /, *, +, - :  ");
+        String operacao = le.next();
+
+
+          switch (operacao) {
+            case "+":
+                int resultado = num1 + num2;
+                System.out.println (" O resultado é:  "+ resultado);
+                break;
+            case "-": 
+                int resultado2 = num1 - num2;
+                System.out.println (" O resultado é:  "+ resultado2);
+                break;
+            case "*": 
+                int resultado3 = num1 * num2;
+                System.out.println (" O resultado é:  "+ resultado3);
+                break;
+            case "/": 
+                int resultado4 = num1 / num2;
+                System.out.println (" O resultado é:  "+ resultado4);
+                break;
+            default: 
+                System.out.println ("Operação invalida");
+                break;
+        }
+    }
+}
